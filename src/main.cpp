3ae2dcba5b7076e900a2d3588859dfd8a5c6961a
BlockBuilder57 <newbrickbuilder7@gmail.com>
2024-11-16 00:47:52 -0600

    FFTW implementation test
    Definitely lots left to do, but the plot is identifiable as a signal, so there's that. Much cleanup to do.

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,10 @@
 #include <SSTV.hpp>
 
 #include <cargs.h>
+#include <fftw3.h>
 #include <SDL3/SDL.h>
 
+#include <complex>
 #include <fstream>
 #include <filesystem>
 
@@ -56,7 +58,7 @@ int main(int argc, char** argv) {
 	fasstv::LogDebug("{}", SDL_VERSION);
 	fasstv::LogDebug("{}", SDL_GetRevision());
 
-	if (!SDL_Init(SDL_INIT_CAMERA)) {
+	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_CAMERA)) {
 		fasstv::LogError("Couldn't initialize SDL: {}", SDL_GetError());
 		return SDL_APP_FAILURE;
 	}
@@ -115,6 +117,13 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	const int windowDimensions[2] = {1024, 512};
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+	SDL_CreateWindowAndRenderer("fasstv", windowDimensions[0], windowDimensions[1], 0, &window, &renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+
 	/*int devcount = 0;
 	SDL_CameraID* devices = SDL_GetCameras(&devcount);
 	if (devices == NULL) {
@@ -152,11 +161,11 @@ int main(int argc, char** argv) {
 
 	// load in image, get proper dimentions
 	fasstv::SSTV::Mode* mode = sstv.GetMode();
-	SDL_Surface* surfOrig = nullptr;
+	SDL_Surface* surfOrig = SDL_CreateSurface(256, 256, SDL_PIXELFORMAT_RGBA32);
 
-	surfOrig = fasstv::LoadImage(inputPath);
-	if (surfOrig == nullptr)
-		return EXIT_FAILURE;
+	//surfOrig = fasstv::LoadImage(inputPath);
+	//if (surfOrig == nullptr)
+	//	return EXIT_FAILURE;
 
 	/*SDL_free(surfOrig);
 	surfOrig = nullptr;
@@ -187,7 +196,7 @@ int main(int argc, char** argv) {
 
 	SDL_free(surfOut);
 
-	std::string extension = ".wav";
+	/*std::string extension = ".wav";
 	if (outputPath.empty()) {
 		outputPath = inputPath;
 		outputPath.replace_filename(inputPath.filename().string() + " " + sstv.GetMode()->name + extension);
@@ -199,7 +208,67 @@ int main(int argc, char** argv) {
 		fasstv::SamplesToWAV(samples, samplerate, file);
 	else
 		fasstv::SamplesToAVCodec(samples, samplerate, file);
-	file.close();
+	file.close();*/
+
+
+	SDL_SetRenderDrawColor(renderer, 80, 80, 80, 255);
+	const int reflines[4] = {1100, 2300, 5, 5};
+	for (int num : reflines) {
+		for (int i = 0; i < 1024; i+=2)
+			SDL_RenderPoint(renderer, i, windowDimensions[1]-(num/8.f));
+	}
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderPresent(renderer);
+
+	const int slideyLen = 128;
+	int line = 0;
+	const int lineSpread = 1;
+	const float binSize = samplerate/slideyLen;
+
+	fftw_complex* out;
+	fftw_plan p;
+
+	std::array<double, slideyLen> doubleSamples {};
+
+	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * slideyLen);
+	p = fftw_plan_dft_r2c_1d(slideyLen, doubleSamples.data(), out, FFTW_ESTIMATE);
+
+	for (int i = 0; i < samples.size(); i += slideyLen) {
+		for (int j = 0; j < slideyLen; j++)
+			doubleSamples[j] = samples[std::min(i+j, (int)samples.size())];
+
+		fftw_execute(p);
+
+		float bestMag = -1;
+		int bestMaxIdx = -1;
+
+		for (int j = 0; j < slideyLen/2; j++) {
+			auto thing = reinterpret_cast<std::complex<double>*>(out[j]);
+			if (thing->imag() >= bestMag) {
+				bestMag = thing->imag();
+				bestMaxIdx = j;
+			}
+		}
+
+		bestMag = 255.f;
+		SDL_SetRenderDrawColor(renderer, bestMag, bestMag, bestMag, 255);
+
+		for (int k = 0; k < lineSpread; k++)
+			SDL_RenderPoint(renderer, line+k, windowDimensions[1]-(bestMaxIdx * binSize / 8.f));
+
+		line += lineSpread;
+		SDL_RenderPresent(renderer);
+	}
+
+	fftw_destroy_plan(p);
+	fftw_free(out);
+
+	SDL_Event event;
+	while (1) {
+		if (SDL_PollEvent(&event) && event.type == SDL_EVENT_QUIT)
+			break;
+	}
 
 	return EXIT_SUCCESS;
 }
